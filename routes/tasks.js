44a94ec2e0c8415e0db9dc913646ad74c3ef68d9
swamp09 <shu.numata@esm.co.jp>
2020-06-16 21:49:48 +0900

    タスクをinsertにできるようにした
    Co-authored-by: appcot <appcot.2013@gmail.com>

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,25 +2,29 @@ var express = require('express');
 const Router = require('express-promise-router');
 const router = new Router();
 const { Pool } = require('pg');
-let pool;
 const env = process.env;
 
-function init() {
-  pool = new Pool({
-    user: env.PGUSER,
-    host: env.PGHOST,
-    database: env.PGDB,
-    password: env.PGPW,
-    port: 5432
-  });
-}
+const pool = new Pool({
+  user: env.PGUSER,
+  host: env.PGHOST,
+  database: env.PGDB,
+  password: env.PGPW,
+  port: 5432
+});
+
 router.get('/', async (req, res, next) => {
-  init();
   let tasks
   await pool.query('select * from public.tasks')
             .then(res => tasks = res.rows[0]);
-
   res.send(tasks);
 });
+
+router.post('/', async (req, res) => {
+  let task
+  let date = new Date();
+  await pool.query("insert into public.tasks(title, completed, created_at) values($1, $2, $3)", [req.body.title, req.body.completed, Math.floor(date.getTime() / 1000)])
+            .then(res => task = res.rows[0]);
+  res.send(task)
+})
 
 module.exports = router;
