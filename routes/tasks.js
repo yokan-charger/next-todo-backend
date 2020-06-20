aeb54c63edddb13b397f8b80c6e53553dadc52ad
swamp09 <shu.numata@esm.co.jp>
2020-06-20 15:40:13 +0900

    CORSを許可する
    Co-authored-by: appcot <appcot.2013@gmail.com>

--- routes/tasks.js
+++ routes/tasks.js
@@ -16,16 +16,13 @@ router.get('/', async (req, res, next) => {
   let tasks
   await pool.query('select * from public.tasks')
             .then(res => tasks = res.rows);
-  console.log("=======")
-  console.log(tasks)
-  console.log("=======")
   res.send(tasks);
 });
 
 router.post('/', async (req, res) => {
   let task
   let date = new Date();
-  await pool.query("insert into public.tasks(title, completed, created_at) values($1, $2, $3)", [req.body.title, req.body.completed, Math.floor(date.getTime() / 1000)])
+  await pool.query("insert into public.tasks(title, completed, created_at) values($1, $2, $3)", [req.body.title, false, Math.floor(date.getTime() / 1000)])
             .then(res => task = res.rows[0]);
   res.send(task)
 })
